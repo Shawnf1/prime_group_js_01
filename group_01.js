5473c0d865905a7df1a6739755d8c4266cd2b0ce
TShaunik <shawnf1@gmail.com>
2015-08-13 12:59:03 -0500

    obj branch

--- group_01.js
+++ group_01.js
@@ -1,98 +1,110 @@
-var arrayAtticus = ["Atticus", "2405", "47000", 3];
-var arrayJem = ["Jem", "62347", "63500", 4];
-var arrayBoo = ["Boo", "11435", "54000", 3];
-var arrayScout = ["Scout", "6243", "74750", 5];
+function Employee(name, number, salary, rating) {
+	this.name = name;
+	this.number = number;
+	this.salary = salary;
+	this.rating = rating;
+	this.percent;
+	this.adjusted;
+	this.bonus;
 
-var employeesArray = [];
-employeesArray.push(consumeArray(arrayAtticus));
-employeesArray.push(consumeArray(arrayJem));
-employeesArray.push(consumeArray(arrayBoo));
-employeesArray.push(consumeArray(arrayScout));
 
-for(var i = 0; i < employeesArray.length; i++)
-{
-	console.log("Employee "+ (i + 1) +" is "+ employeesArray[i]["name"] +", who will get a "+ employeesArray[i]["percent"] +"% raise, which will raise his/her income to $"+ commatize(employeesArray[i]["adjusted"]) +", with his/her bonus being $"+ commatize(employeesArray[i]["bonus"]) +".");
-}
-
-function commatize(num)
-{
-	var string = num.toString();
-	var newString = "";
-	if(string.length > 3)
-	{
-		for(var i = 0; i < string.length; i++)
-		{
-			if((string.length - i) % 3 == 0)
-			{
-				newString += ","+ string[i];
-			}else
-			{
-				newString += string[i];
-			}
+	this.CalcSTI = function (number, salary, rating) {
+		var percent;
+		switch (rating) {
+			case 5:
+				percent = .1;
+				break;
+			case 4:
+				percent = .06;
+				break;
+			case 3:
+				percent = .04;
+				break;
+			default:
+				percent = 0;
+				break;
 		}
-	}else
-	{
-		newString = string;
+		if (number.length == 4) {
+			percent += .05;
+		}
+		if (salary > 65000) {
+			percent -= .01;
+		}
+		if (percent > .13) {
+			percent = .13;
+		}
+		return this.percent = percent;
+	};
+
+	this.calcAdjusted = function (salary, percent) {
+		return this.adjusted = Math.round(salary *= (1 + percent));
+	};
+
+	this.calcBonus = function (salary, percent) {
+		return this.bonus = Math.round(salary * percent);
+	};
+
+
+	this.CalcSTI(this.number, this.salary, this.rating);
+	this.calcAdjusted(this.salary, this.percent);
+	this.calcBonus(this.salary, this.percent);
+};
+
+function Company() {
+	this.employees = [];
+	this.output = new Output();
+	this.AddEmployee = function (array) {
+		this.employees.push(array);
+	};
+
+	this.ShowEmployeeComp = function (employee) {
+		var msg = employee.name + " will get a " +
+			employee.percent + "% raise, which will raise his/her income to $" +
+			employee.adjusted.toLocaleString() + ", with his/her bonus being $" +
+			employee.bonus.toLocaleString() + ".";
+		return msg;
+	};
+
+	this.ShowAllEmployees = function () {
+		this.output.CreateUL("employeeComp_ul");
+		for (var i = 0; i < this.employees.length; i++) {
+			this.output.CreateLI(this.ShowEmployeeComp(this.employees[i]));
+		}
+		this.output.EndUL();
 	}
-	return newString;
+
+	this.GetOutput = function () {
+		return this.output.GetOutput();
+	}
+};
+
+function Output() {
+	this.html = "";
+	this.CreateUL = function (id) {
+		this.html += "<ul";
+		this.html += " id='"+ id +"'>" || ">";
+	}
+	this.CreateLI = function(msg)
+	{
+		this.html += "<li>" + msg + "</li>";
+	}
+	this.EndUL = function () {
+		this.html += "</ul>";
+	}
+	this.GetOutput = function() {
+		return this.html;
+	}
 }
 
-function consumeArray(oldArray)
-{
-	var newArray = [];
-	// copy name over
-	newArray["name"] = oldArray[0];
-	// calculate percent, then adjusted, then bonus
-	var results = calcSTI(oldArray[1], parseInt(oldArray[2]), oldArray[3]);
-	console.log(newArray["name"], results);
-	newArray["percent"] = results["percent"];
-	newArray["adjusted"] = results["adjusted"];
-	newArray["bonus"] = results["bonus"];
-	return newArray;
-}
+var atticus = new Employee("Atticus", "2405", "47000", 3);
+var jem = new Employee("Jem", "62347", "63500", 4);
+var boo = new Employee("Boo", "11435", "54000", 3);
+var scout = new Employee("Scout", "6243", "74750", 5);
+var company = new Company();
+company.AddEmployee(atticus);
+company.AddEmployee(jem);
+company.AddEmployee(boo);
+company.AddEmployee(scout);
+company.ShowAllEmployees();
 
-function calcSTI(num, income, rating)
-{
-	var percent;
-	var adjusted;
-	var bonus;
-	switch (rating)
-	{
-		case 5:
-			percent = 10;
-			adjusted = income * 1.1;
-			bonus = income * .1;
-			break;
-		case 4:
-			percent = 6;
-			adjusted = income * 1.06;
-			bonus = income * .06;
-			break;
-		case 3:
-			percent = 4;
-			adjusted = income * 1.04;
-			bonus = income * .04;
-			break;
-		default:
-			percent = 0;
-			adjusted = income;
-			bonus = 0;
-			break;
-	}
-	if(num.length == 4)
-	{
-		percent += 5;
-	}
-	if(income > 65000)
-	{
-		percent--;
-	}
-	if(percent > 13)
-	{
-		percent = 13;
-	}
-	adjusted =  Math.round(income * ((100 + percent) / 100));
-	bonus = Math.round(income * percent / 100);
-	//var result = []
-	return {"percent" : percent, "adjusted" : adjusted, "bonus" : bonus};
-}
+$('#employees').append(company.GetOutput());
